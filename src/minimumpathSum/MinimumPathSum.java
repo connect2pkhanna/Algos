1f8b0614d39218e0be3e9b694ab6bb64609d1f3e
Puneet <khanna-puneet@hotmail.com>
2019-11-03 10:40:43 +0530

    leetcode.com/problems/minimum-path-sum/

---
+++ src/minimumpathSum/MinimumPathSum.java
@@ -0,0 +1,76 @@
+package minimumpathSum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//https://leetcode.com/problems/minimum-path-sum/
+public class MinimumPathSum {
+	private static int minSum = Integer.MAX_VALUE;
+
+	public static void main(String[] args) {
+		int[][] arr = { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } };
+
+		dfs(arr, new Position(0, 0), arr[0][0]);
+		System.out.println(minSum);
+	}
+
+	public static void dfs(int[][] arr, Position pos, int sum) {
+		
+		List<Position> positions = getNextValidPositions(pos.x, pos.y, arr);
+
+		for (Position position : positions) {
+
+			if (position.x == arr.length - 1 && position.y == arr[0].length - 1) {
+				minSum = Math.min(minSum, sum);
+				// there is nowhere to go from here
+				return;
+			}
+			dfs(arr, position, sum + arr[position.x][position.y]);
+
+		}
+
+	}
+
+	private static List<Position> getNextValidPositions(int i, int j, int[][] arr) {
+		List<Position> positions = new ArrayList<Position>();
+		Position down = new Position(i + 1, j);
+		Position right = new Position(i, j + 1);
+		if (isValid(down, arr)) {
+			positions.add(down);
+		}
+		if (isValid(right, arr)) {
+			positions.add(right);
+		}
+
+		return positions;
+	}
+
+	private static boolean isValid(Position pos, int[][] arr) {
+		int i = pos.x;
+		int j = pos.y;
+
+		if (i < 0 || i > arr.length - 1 || j < 0 || j > arr[0].length - 1) {
+			return false;
+		}
+		return true;
+	}
+
+}
+
+class Position {
+
+	int x;
+	int y;
+
+	public Position(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Position [x=" + x + ", y=" + y + "]";
+	}
+
+}
