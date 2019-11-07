6f8c3b584a429557a1e2217796d9238a5e9e71f0
Puneet <khanna-puneet@hotmail.com>
2019-11-07 22:44:34 +0530

    fix to consider odd nums instead of 1
    fix to not stop recursion when solution is found but iterate through all
    sombinations

--- src/subetsWithADefiniteParity/SubetsWithParityK.java
+++ src/subetsWithADefiniteParity/SubetsWithParityK.java
@@ -4,15 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 //https://leetcode.com/problems/count-number-of-nice-subarrays/
-
-/*The question is similar to finding subets(sequential) but we have to keep a track of count of 1s find in the subset*/
+/*The question is similar to finding subets(sequential) but we have to keep a track of count of odds find in the subset*/
 public class SubetsWithParityK {
+	static List<List<Integer>> results= new ArrayList<List<Integer>>();
 public static void main(String[] args) {
-	int[] arr= { 1,1,2,1,1};
+	int[] arr= {2,2,2,1,2,2,1,2,2,2};
+	
 	List<Integer> list= new ArrayList<Integer>();
 	for (int i = 0; i <=arr.length-1; i++) {
-		findSubSetsWithkOnes(arr,i,0,list,3);
+		findSubSetsWithkOnes(arr,i,0,list,2);
 	}
+	System.out.println(results.size());
 	
 }
 
@@ -22,16 +24,16 @@ private static void findSubSetsWithkOnes(int[] arr, int i, int sum,List<Integer>
 		for (Integer num : list) {
 			System.out.print(num);
 		}
-		System.out.println("----");
-		list.clear();
-		return ;
+		System.out.println("====");
+		results.add(list);
+		
 	}
 	if(i>arr.length-1) {
 		list.clear();
 		return;
 	}
 	
-	if(arr[i]==1) {
+	if((arr[i] & 1)==1) {
 		sum=sum+1;
 	}
 	list.add(arr[i]);
